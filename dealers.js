ff83fc24a34788dc1f07fc0b2107e7e346d3e67d
Max <gonzjv@ya.ru>
2021-10-05 15:58:33 +0300

    Feature/kick by voting (#5)
    * feat: kicked by dealer notification
    
    * feat: kick player by voting
    
    * fix: dealer and myself delete buttons
    
    * feat: start game

--- dealers.js
+++ dealers.js
@@ -1,9 +1,9 @@
 const dealers = [];
 
-export const addDealer = (lobbyID, firstName, lastName, jobPosition) => {
+export const addDealer = (id, lobbyID, firstName, lastName, jobPosition) => {
   if (!firstName) return { error: 'First name is required' };
 
-  const dealer = { lobbyID, firstName, lastName, jobPosition };
+  const dealer = { id, lobbyID, firstName, lastName, jobPosition };
   dealers.push(dealer);
 
   return { dealer };
--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const io = new Server(httpServer, {});
 import { addDealer, getDealer } from './dealers.js';
 import { createGame } from './games.js';
 import { addPlayer, deletePlayer, getPlayer, getPlayers } from './players.js';
+import { addVoting, getVoting, addVote, resetVoting } from './voting.js';
 
 app.use(cors());
 io.on('connection', (socket) => {
@@ -50,7 +51,13 @@ io.on('connection', (socket) => {
   });
   socket.on('createGame', ({ firstName, lastName, jobPosition }, callback) => {
     const game = createGame(socket.id);
-    const { dealer, error } = addDealer(socket.id, firstName, lastName, jobPosition);
+    const { dealer, error } = addDealer(
+      socket.id,
+      socket.id,
+      firstName,
+      lastName,
+      jobPosition,
+    );
     if (error) {
       return callback(error);
     }
@@ -62,19 +69,44 @@ io.on('connection', (socket) => {
     const user = getPlayer(socket.id) || getDealer(socket.id);
     io.in(user.lobbyID).emit('message', { user: user.firstName, text: message });
   });
-  socket.on('deletePlayer', (id, dealerID) => {
-    const deleteByDealer = (playerID) => {
-      const player = deletePlayer(playerID);
-      io.in(player.lobbyID).emit('players', getPlayers(player.lobbyID));
+  socket.on('deletePlayer', (id) => {
+    const player = deletePlayer(id);
+    io.in(player.lobbyID).emit('players', getPlayers(player.lobbyID));
+    io.to(player.id).emit('kickFromLobby');
+    io.in(player.lobbyID).emit(
+      'notification',
+      `${player.firstName} kicked by dealer`,
+    );
+  });
+  socket.on('startVoting', (player, initiatorID) => {
+    const initiator = getPlayer(initiatorID);
+    const votersCount = getPlayers(initiator.lobbyID).length;
+    const voting = addVoting(initiatorID, 1, votersCount);
+    socket
+      .in(player.lobbyID)
+      .except(player.id)
+      .emit('votingPopup', player, initiator);
+  });
+  socket.on('vote', (votingID, player) => {
+    const voting = addVote(votingID);
+    socket.emit('voteCount', voting);
+    const kickPlayer = () => {
+      deletePlayer(player.id);
       io.to(player.id).emit('kickFromLobby');
+      io.in(player.lobbyID).emit('players', getPlayers(player.lobbyID));
+      io.in(player.lobbyID).emit(
+        'notification',
+        `${player.firstName} kicked by voting`,
+      );
+      io.in(player.lobbyID).emit('closeVoting');
+      resetVoting(votingID);
     };
-
-    socket.id === dealerID
-      ? deleteByDealer(id)
-      : console.log('you are not a dealer, little nerd!');
+    voting.count >= voting.approveCount ? kickPlayer() : {};
+  });
+  socket.on('startGame', (lobbyID) => {
+    io.in(lobbyID).emit('dealerStartGame');
   });
 });
-
 app.get('/', (req, res) => {
   res.send('Server is up and running');
 });
