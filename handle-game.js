dbcbcc0b0ac68d8fc093d103aacbaa38c9b0c04b
gonzjv <gonzjv@ya.ru>
2021-10-20 11:45:11 +0300

    feat: results page

--- handle-game.js
+++ handle-game.js
@@ -1,6 +1,22 @@
+import { getGameVotes } from './game-voting.js';
+
 const handleGame = (socket, io) => {
+  socket.on('startGame', (lobbyID) => {
+    io.in(lobbyID).emit('dealerStartGame');
+  });
+
   socket.on('startRound', (lobbyID) => {
     io.in(lobbyID).emit('dealerStartRound');
+  });
+
+  socket.on('addGameVote', (lobbyID, issueID, cardValue) => {
+    addGameVote(lobbyID, socket.id, issueID, cardValue);
+    socket.join(lobbyID);
+    io.in(lobbyID).emit('sendVotes', getGameVotes(issueID));
+  });
+
+  socket.on('stopGame', (lobbyID) => {
+    io.in(lobbyID).emit('dealerStopGame');
   });
 };
 
